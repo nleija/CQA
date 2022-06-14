1b23c8527f9a6f8a7d4ce822a5544ddd8aad3299
Norma Leija <norma.leija@wizeline.com>
2022-06-14 11:29:45 -0500

    update gitignore

--- tests/Session1/firstFile.js
+++
@@ -1,9 +0,0 @@
-import { fixture } from "testcafe"
-
-fixture ("firstTest")
-
-test
-	.page 'http://demoqa.com/text-box';
-	("Demo QA site", async t => {
-		console.log("Se ingreso a la pagina de DemoQA correctamente")
-	})
